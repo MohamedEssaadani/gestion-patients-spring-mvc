0c783bd2218fa4bd8e01b119aaaad4df40adce4d
Mohamed Es-saadani <essaadani80@gmail.com>
2021-05-01 01:19:11 +0000

    'commit'

--- src/main/java/com/essaadani/springmvc/web/PatientsController.java
+++ src/main/java/com/essaadani/springmvc/web/PatientsController.java
@@ -57,6 +57,7 @@ public class PatientsController {
         model.addAttribute("medecinList", medecinList);
         model.addAttribute("patient", new Patient());
         model.addAttribute("mode", "new");
+        //Ss
         return "formPatient";
     }
 
