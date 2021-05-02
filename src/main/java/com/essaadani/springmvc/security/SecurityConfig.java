6191ab2ae7d0c9e614f59ff2c94a77e3d61fe00d
Mohamed Es-saadani <essaadani80@gmail.com>
2021-05-02 01:10:12 +0000

    jdbc auth

--- src/main/java/com/essaadani/springmvc/security/SecurityConfig.java
+++ src/main/java/com/essaadani/springmvc/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.essaadani.springmvc.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -8,19 +9,31 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    DataSource dataSource;
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         PasswordEncoder passwordEncoder = passwordEncoder();
-        auth.inMemoryAuthentication().withUser("mohamed").password(passwordEncoder.encode("123")).roles("USER");
-        auth.inMemoryAuthentication().withUser("admin").password(passwordEncoder.encode("123")).roles("USER", "ADMIN");
+        System.out.println(passwordEncoder.encode("1234"));
+       /* auth.inMemoryAuthentication().withUser("mohamed").password(passwordEncoder.encode("123")).roles("USER");
+        auth.inMemoryAuthentication().withUser("admin").password(passwordEncoder.encode("123")).roles("USER", "ADMIN");*/
+
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery("SELECT username as principal, password as credentials, active FROM users WHERE username = ?")
+                .authoritiesByUsernameQuery("SELECT username as principal, role as role FROM users_roles WHERE username = ?")
+                .rolePrefix("ROLE_")
+                .passwordEncoder(passwordEncoder);
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.formLogin();
-        http.authorizeRequests().antMatchers("admin**/**", "/save**/**", "/delete**/**", "/form**/**").hasRole("ADMIN");
+        http.authorizeRequests().antMatchers("/admin/**", "/save**/**", "/delete**/**", "/form**/**").hasRole("ADMIN");
         http.authorizeRequests().antMatchers("/patients**/**").hasRole("USER");
         http.authorizeRequests().antMatchers("/user**/**").permitAll();
         http.authorizeRequests().anyRequest().authenticated();
