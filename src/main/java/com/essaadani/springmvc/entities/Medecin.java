3abee4c9fdf49bca3badcde333e73a5e741e5a2e
Mohamed Es-saadani <essaadani80@gmail.com>
2021-05-06 14:34:41 +0000

    'commit'

--- src/main/java/com/essaadani/springmvc/entities/Medecin.java
+++ src/main/java/com/essaadani/springmvc/entities/Medecin.java
@@ -4,8 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Entity
@@ -13,8 +15,10 @@ import java.util.Date;
 public class Medecin {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Size(min=5, max=15)
     private String nom;
     @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date dateNaissance;
     private String specialite;
 
--- src/main/java/com/essaadani/springmvc/web/MedecinsController.java
+++ src/main/java/com/essaadani/springmvc/web/MedecinsController.java
@@ -3,14 +3,19 @@ package com.essaadani.springmvc.web;
 import com.essaadani.springmvc.entities.Medecin;
 import com.essaadani.springmvc.entities.Patient;
 import com.essaadani.springmvc.repositories.MedecinRepository;
+import org.dom4j.rule.Mode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.validation.Valid;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -40,5 +45,45 @@ public class MedecinsController {
         medecinRepository.deleteById(id);
 
         return "redirect:/medecins?page="+page+"&keyword="+keyword+"&size="+size;
+    }
+
+    @GetMapping("/formMedecin")
+    public String formMedecin(Model model){
+        List<String> specialiteList =  new ArrayList<>();
+        specialiteList.add("Géneral");
+        specialiteList.add("Psychiatre");
+
+        model.addAttribute("mode", "new");
+        model.addAttribute("medecin", new Medecin());
+        model.addAttribute("specialiteList", specialiteList);
+
+        return "formMedecin";
+    }
+
+    @PostMapping("/saveMedecin")
+    public String saveMedecin(@Valid Medecin medecin, BindingResult bindingResult, Model model){
+        if(bindingResult.hasErrors())
+            return "formMedecin";
+
+        medecinRepository.save(medecin);
+
+        model.addAttribute("medecin", medecin);
+
+        return "confirmationMedecin";
+
+    }
+
+    @GetMapping("/editMedecin")
+    public String editMedecin(@RequestParam Long id, Model model){
+        List<String> specialiteList =  new ArrayList<>();
+        specialiteList.add("Géneral");
+        specialiteList.add("Psychiatre");
+
+        Medecin medecin = medecinRepository.findById(id).get();
+        model.addAttribute("mode", "edit");
+        model.addAttribute("medecin", medecin);
+        model.addAttribute("specialiteList", specialiteList);
+
+        return "formMedecin";
     }
 }
